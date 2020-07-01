e05eab65fac13a6cf683bbc2e20e65f2e1c43111
Wesley <wtp_2020@163.com>
2020-07-01 17:18:24 +0800

    [mod]适配选股调度引擎增加以后引起的函数名修改，以及兼容调用方式

--- wtpy/Context.py
+++ wtpy/Context.py
@@ -113,14 +113,14 @@ class Context:
         输出日志
         @message    消息内容\n
         '''
-        self.__wrapper__.ctx_str_log_text(self.__id__, message)
+        self.__wrapper__.cta_log_text(self.__id__, message)
         
     def stra_get_date(self):
         '''
         获取当前日期\n
         @return int，格式如20180513
         '''
-        return self.__wrapper__.ctx_str_get_date()
+        return self.__wrapper__.cta_get_date()
 
     def stra_get_position_avgpx(self, code:str = ""):
         '''
@@ -128,7 +128,7 @@ class Context:
         @code   合约代码
         @return 持仓均价
         '''
-        return self.__wrapper__.ctx_str_get_position_avgpx(self.__id__, code)
+        return self.__wrapper__.cta_get_position_avgpx(self.__id__, code)
 
     def stra_get_position_profit(self, code:str = ""):
         '''
@@ -136,21 +136,21 @@ class Context:
         @code   合约代码，为None时读取全部品种的浮动盈亏
         @return 浮动盈亏
         '''
-        return self.__wrapper__.ctx_str_get_position_profit(self.__id__, code)
+        return self.__wrapper__.cta_get_position_profit(self.__id__, code)
 
     def stra_get_time(self):
         '''
         获取当前时间，24小时制，精确到分\n
         @return int，格式如1231
         '''
-        return self.__wrapper__.ctx_str_get_time()
+        return self.__wrapper__.cta_get_time()
 
     def stra_get_price(self, code):
         '''
         获取最新价格，一般在获取了K线以后再获取该价格
         @return 最新价格
         '''
-        return self.__wrapper__.ctx_str_get_price(code)
+        return self.__wrapper__.cta_get_price(code)
 
     def stra_get_bars(self, code:str, period:str, count:int, isMain:bool = False):
         '''
@@ -166,7 +166,7 @@ class Context:
             #这里做一个数据长度处理
             return self.__bar_cache__[key].iloc[-count:]
 
-        cnt =  self.__wrapper__.ctx_str_get_bars(self.__id__, code, period, count, isMain)
+        cnt =  self.__wrapper__.cta_get_bars(self.__id__, code, period, count, isMain)
         if cnt == 0:
             return None
 
@@ -181,7 +181,7 @@ class Context:
         @code   合约代码
         @count  要拉取的tick数量
         '''
-        cnt = self.__wrapper__.ctx_str_get_ticks(self.__id__, code, count)
+        cnt = self.__wrapper__.cta_get_ticks(self.__id__, code, count)
         if cnt == 0:
             return None
         
@@ -195,7 +195,7 @@ class Context:
         @usertag    入场标记
         @return     正为多仓，负为空仓
         '''
-        return self.__wrapper__.ctx_str_get_position(self.__id__, code, usertag)
+        return self.__wrapper__.cta_get_position(self.__id__, code, usertag)
 
     def stra_set_position(self, code:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
         '''
@@ -204,7 +204,7 @@ class Context:
         @qty    目标仓位，正为多仓，负为空仓\n
         @return 设置结果TRUE/FALSE
         '''
-        self.__wrapper__.ctx_str_set_position(self.__id__, code, qty, usertag, limitprice, stopprice)
+        self.__wrapper__.cta_set_position(self.__id__, code, qty, usertag, limitprice, stopprice)
         
 
     def stra_enter_long(self, code:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
@@ -215,7 +215,7 @@ class Context:
         @limitprice 限价，默认为0\n
         @stopprice  止价，默认为0
         '''
-        self.__wrapper__.ctx_str_enter_long(self.__id__, code, qty, usertag, limitprice, stopprice)
+        self.__wrapper__.cta_enter_long(self.__id__, code, qty, usertag, limitprice, stopprice)
 
     def stra_exit_long(self, code:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
         '''
@@ -225,7 +225,7 @@ class Context:
         @limitprice 限价，默认为0\n
         @stopprice  止价，默认为0
         '''
-        self.__wrapper__.ctx_str_exit_long(self.__id__, code, qty, usertag, limitprice, stopprice)
+        self.__wrapper__.cta_exit_long(self.__id__, code, qty, usertag, limitprice, stopprice)
 
     def stra_enter_short(self, code:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
         '''
@@ -235,7 +235,7 @@ class Context:
         @limitprice 限价，默认为0\n
         @stopprice  止价，默认为0
         '''
-        self.__wrapper__.ctx_str_enter_short(self.__id__, code, qty, usertag, limitprice, stopprice)
+        self.__wrapper__.cta_enter_short(self.__id__, code, qty, usertag, limitprice, stopprice)
 
     def stra_exit_short(self, code:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
         '''
@@ -245,7 +245,7 @@ class Context:
         @limitprice 限价，默认为0\n
         @stopprice  止价，默认为0
         '''
-        self.__wrapper__.ctx_str_exit_short(self.__id__, code, qty, usertag, limitprice, stopprice)
+        self.__wrapper__.cta_exit_short(self.__id__, code, qty, usertag, limitprice, stopprice)
 
     def stra_get_last_entrytime(self, code:str):
         '''
@@ -253,7 +253,7 @@ class Context:
         @code   品种代码\n
         @return 返回最后一次开仓的时间，格式如201903121047
         '''
-        return self.__wrapper__.ctx_str_get_last_entertime(self.__id__, code)
+        return self.__wrapper__.cta_get_last_entertime(self.__id__, code)
 
     def stra_get_first_entrytime(self, code:str):
         '''
@@ -261,7 +261,7 @@ class Context:
         @code   品种代码\n
         @return 返回最后一次开仓的时间，格式如201903121047
         '''
-        return self.__wrapper__.ctx_str_get_first_entertime(self.__id__, code)
+        return self.__wrapper__.cta_get_first_entertime(self.__id__, code)
 
 
     def user_save_data(self, key:str, val):
@@ -270,7 +270,7 @@ class Context:
         @key    数据id
         @val    数据值，可以直接转换成str的数据均可
         '''
-        self.__wrapper__.ctx_str_save_user_data(self.__id__, key, str(val))
+        self.__wrapper__.cta_save_user_data(self.__id__, key, str(val))
 
     def user_load_data(self, key:str, defVal = None, vType = float):
         '''
@@ -279,7 +279,7 @@ class Context:
         @defVal 默认数据，如果找不到则返回改数据，默认为None
         @return 返回值，默认处理为float数据
         '''
-        ret = self.__wrapper__.ctx_str_load_user_data(self.__id__, key, "")
+        ret = self.__wrapper__.cta_load_user_data(self.__id__, key, "")
         if ret == "":
             return defVal
 
@@ -293,7 +293,7 @@ class Context:
         @flag       盈亏记号，0-浮动盈亏，1-最大浮盈，2-最大亏损（负数）
         @return     盈亏 
         '''
-        return self.__wrapper__.ctx_str_get_detail_profit(self.__id__, code, usertag, flag)
+        return self.__wrapper__.cta_get_detail_profit(self.__id__, code, usertag, flag)
 
     def stra_get_detail_cost(self, code:str, usertag:str):
         '''
@@ -302,7 +302,7 @@ class Context:
         @usertag    进场标记\n
         @return     开仓价 
         '''
-        return self.__wrapper__.ctx_str_get_detail_cost(self.__id__, code, usertag)
+        return self.__wrapper__.cta_get_detail_cost(self.__id__, code, usertag)
 
     def stra_get_detail_entertime(self, code:str, usertag:str):
         '''
@@ -311,7 +311,7 @@ class Context:
         @usertag    进场标记\n
         @return     进场时间，格式如201907260932 
         '''
-        return self.__wrapper__.ctx_str_get_detail_entertime(self.__id__, code, usertag)
+        return self.__wrapper__.cta_get_detail_entertime(self.__id__, code, usertag)
 
     def stra_get_comminfo(self, code:str):
         '''
