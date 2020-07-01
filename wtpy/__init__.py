26ec4b921fccf1b2c3d6468e9db528e879540462
Wesley <wtp_2020@163.com>
2020-07-01 17:17:50 +0800

    [add]新增一个选股策略环境模块SelContext，以及BaseDefs里增加一个选股策略基类定义BaseSelStrategy

--- wtpy/__init__.py
+++ wtpy/__init__.py
@@ -1,9 +1,11 @@
-from .BaseDefs import BaseStrategy
-from .WtEngine import WtEngine
+from .BaseDefs import BaseStrategy, BaseSelStrategy
 from .Context import Context
+from .SelContext import SelContext
+from .WtEngine import WtEngine
 from .WtBtEngine import WtBtEngine
 from .WtDtEngine import WtDtEngine
 from .ExtDefs import BaseIndexWriter
 from .ExtDefs import BaseDataReporter
+from wtpy.porter.WtExecApi import WtExecApi
 
-__all__ = ["BaseStrategy", "WtEngine", "Context", "WtBtEngine", "BaseIndexWriter", "BaseDataReporter", "WtDtEngine"]
+__all__ = ["BaseStrategy", "BaseSelStrategy", "WtEngine", "Context", "SelContext", "WtBtEngine", "BaseIndexWriter", "BaseDataReporter", "WtDtEngine", "WtExecApi"]
