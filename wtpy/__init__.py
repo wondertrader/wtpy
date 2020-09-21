4c7ec915e126ea101125c4c6d70376c824987fdd
wesley <wondertrader@126.com>
2020-09-21 15:56:26 +0800

    [mod]更新了readme，并重新提交CTPLoader可执行程序

--- wtpy/__init__.py
+++ wtpy/__init__.py
@@ -8,10 +8,12 @@ from .WtBtAnalyst import WtBtAnalyst
 from .WtDtEngine import WtDtEngine
 from .ExtToolDefs import BaseIndexWriter
 from .ExtToolDefs import BaseDataReporter
-from wtpy.wrapper.WtExecApi import WtExecApi
 from .WtCoreDefs import WTSTickStruct,WTSBarStruct,EngineType
 from .WtDataDefs import WtKlineData,WtTickData
 
+from wtpy.wrapper.WtExecApi import WtExecApi
+from wtpy.wrapper.CTPLoader import CTPLoader
+
 __all__ = ["BaseCtaStrategy", "BaseSelStrategy", "BaseHftStrategy", "WtEngine", "CtaContext", "SelContext", "HftContext", 
             "WtBtEngine", "BaseIndexWriter", "BaseDataReporter", "WtDtEngine", "WtExecApi","WTSTickStruct","WTSBarStruct",
-            "EngineType","WtBtAnalyst", "WtKlineData", "WtTickData"]
+            "EngineType","WtBtAnalyst", "WtKlineData", "WtTickData","CTPLoader"]
