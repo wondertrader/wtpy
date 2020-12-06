ba0f4d959407205e441fcf0909c2431c20d67b22
wondertrader <wtp_2020@163.com>
2020-12-06 19:42:39 +0800

    [mod]添加了demo中linux平台的配置文件，方便测试的时候选用

--- demos/cta_fut/run.py
+++ demos/cta_fut/run.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
     env = WtEngine(EngineType.ET_CTA)
     env.init('./common/', "config.json")
     
-    straInfo = StraDualThrust(name='pydt_IF', code="CFFEX.IF.HOT", barCnt=50, period="m5", days=30, k1=0.1, k2=0.1, isForStk=False)
-    env.add_cta_strategy(straInfo)
+    # straInfo = StraDualThrust(name='pydt_IF', code="CFFEX.IF.HOT", barCnt=50, period="m5", days=30, k1=0.1, k2=0.1, isForStk=False)
+    # env.add_cta_strategy(straInfo)
     
+    straInfo = StraDualThrust(name='pydt_au', code="SHFE.au.HOT", barCnt=50, period="m5", days=30, k1=0.2, k2=0.2, isForStk=False)
+    env.add_cta_strategy(straInfo)
+
     env.run()
 
     kw = input('press any key to exit\n')
