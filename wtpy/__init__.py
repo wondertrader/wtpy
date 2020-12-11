2a480516ce21466e064ad707f627901064c5217f
wesley <wondertrader@126.com>
2020-12-11 15:52:05 +0800

    [mod]C++底层更新到dev最新版本（截止到2020/12/11 15:52）

--- wtpy/__init__.py
+++ wtpy/__init__.py
@@ -4,17 +4,14 @@ from .SelContext import SelContext
 from .HftContext import HftContext
 from .WtEngine import WtEngine
 from .WtBtEngine import WtBtEngine
-from .WtBtAnalyst import WtBtAnalyst
 from .WtDtEngine import WtDtEngine
-from .ExtToolDefs import BaseIndexWriter
-from .ExtToolDefs import BaseDataReporter
 from .WtCoreDefs import WTSTickStruct,WTSBarStruct,EngineType
 from .WtDataDefs import WtKlineData,WtTickData
-from .WtOptimizer import WtOptimizer
+from .ExtToolDefs import BaseDataReporter, BaseIndexWriter
 
 from wtpy.wrapper.WtExecApi import WtExecApi
 from wtpy.wrapper.CTPLoader import CTPLoader
 
 __all__ = ["BaseCtaStrategy", "BaseSelStrategy", "BaseHftStrategy", "WtEngine", "CtaContext", "SelContext", "HftContext", 
-            "WtBtEngine", "BaseIndexWriter", "BaseDataReporter", "WtDtEngine", "WtExecApi","WTSTickStruct","WTSBarStruct",
-            "EngineType","WtBtAnalyst", "WtKlineData", "WtTickData","CTPLoader","WtOptimizer"]
+            "WtBtEngine", "WtDtEngine", "WtExecApi","WTSTickStruct","WTSBarStruct","BaseIndexWriter","BaseIndexWriter",
+            "EngineType", "WtKlineData", "WtTickData","CTPLoader"]
