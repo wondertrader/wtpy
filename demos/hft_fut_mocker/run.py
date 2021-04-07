0a2a855827e2a455258777984421e2fd77f92e12
wondertrader <wondertrader@126.com>
2021-04-07 10:32:30 +0800

    [add]新增一个hft配置本地仿真模块mocker的demo，/demos/hft_fut_mocker/

---
+++ demos/hft_fut_mocker/run.py
@@ -0,0 +1,15 @@
+from wtpy import WtEngine,EngineType
+from strategies.HftStraDemo import HftStraDemo
+
+if __name__ == "__main__":
+    #创建一个运行环境，并加入策略
+    engine = WtEngine(EngineType.ET_HFT)
+    engine.init('./common/', "config.json")
+    engine.commitConfig()
+
+    straInfo = HftStraDemo(name="hft_IF", code="CFFEX.IF.2104", expsecs=5, offset=100, freq=0)
+    engine.add_hft_strategy(straInfo, 'mocker')
+    
+    engine.run()
+
+    kw = input('press any key to exit\n')
