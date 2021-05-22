9268b19c25f2007f988358385ae26d6e40a83b80
wondertrader <wondertrader@126.com>
2021-05-22 21:27:06 +0800

    [mod]由于底层将配置文件拆分，所以demo进行了统一更新

--- demos/datakit_fut/runDT.py
+++ demos/datakit_fut/runDT.py
@@ -2,9 +2,9 @@ from wtpy import WtDtEngine
 
 if __name__ == "__main__":
     #创建一个运行环境，并加入策略
-    env = WtDtEngine()
-    env.initialize("dtcfg.json", "logcfgdt.json")
+    engine = WtDtEngine()
+    engine.initialize("dtcfg.json", "logcfgdt.json")
     
-    env.run()
+    engine.run()
 
     kw = input('press any key to exit\n')
