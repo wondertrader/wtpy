64ad04e57fd2b22b81afef7233c64e12b699b0d6
wondertrader <wondertrader@126.com>
2021-06-08 16:24:39 +0800

    [mod]更新了合约加载器的demo

--- demos/ctp_loader/run.py
+++ demos/ctp_loader/run.py
@@ -1,7 +1,5 @@
 from wtpy import ContractLoader,LoaderType
-import os
 
-os.chdir('d:\\aa')
 loader = ContractLoader(lType = LoaderType.LT_CTP)
 loader.start(cfgfile="config.ini")
 input("press enter key to exit\n")
