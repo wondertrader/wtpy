2aa3b10b7d0bcf46c26b821ff1589530aafb4e06
wondertrader <wondertrader@126.com>
2021-06-08 16:24:10 +0800

    [mod]合约加载器修改成可以设置配置文件

--- demos/ctp_loader/run.py
+++ demos/ctp_loader/run.py
@@ -1,5 +1,7 @@
 from wtpy import ContractLoader,LoaderType
+import os
 
+os.chdir('d:\\aa')
 loader = ContractLoader(lType = LoaderType.LT_CTP)
-loader.start()
+loader.start(cfgfile="config.ini")
 input("press enter key to exit\n")
