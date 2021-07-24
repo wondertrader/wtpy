ecfc7a2cd281b0fc29df43523ca82fced6dbdf8d
wondertrader <wondertrader@126.com>
2021-07-24 22:05:40 +0800

    [mod]1、singleton装饰器改成从WtUtilDefs模块导入；2、全局回调函数on_log_output改成成员函数

--- wtpy/wrapper/WtDtHelper.py
+++ wtpy/wrapper/WtDtHelper.py
@@ -2,7 +2,7 @@ from ctypes import cdll, CFUNCTYPE, c_char_p, c_void_p, c_bool, POINTER, c_int, 
 from wtpy.WtCoreDefs import WTSTickStruct, WTSBarStruct, BarList, TickList
 from wtpy.SessionMgr import SessionInfo
 from wtpy.wrapper.PlatformHelper import PlatformHelper as ph
-from copy import copy
+from wtpy.WtUtilDefs import singleton
 import os
 
 CB_DTHELPER_LOG = CFUNCTYPE(c_void_p,  c_char_p)
@@ -20,6 +20,7 @@ def on_log_output(message:str):
 
 cb_dthelper_log = CB_DTHELPER_LOG(on_log_output)
 
+@singleton
 class WtDataHelper:
     '''
     Wt平台数据组件C接口底层对接模块
@@ -49,7 +50,13 @@ class WtDataHelper:
             _path = os.path.join(*a)
             self.api = cdll.LoadLibrary(_path)
 
+        self.cb_dthelper_log = CB_DTHELPER_LOG(on_log_output)
+
         self.api.resample_bars.argtypes = [c_char_p, CB_DTHELPER_BAR, CB_DTHELPER_COUNT, c_uint64, c_uint64, c_char_p, c_uint, c_char_p, CB_DTHELPER_LOG]
+
+    def on_log_output(message:str):
+        message = bytes.decode(message, 'gbk')
+        print(message)
 
     def dump_bars(self, binFolder:str, csvFolder:str, strFilter:str=""):
         '''
@@ -58,7 +65,7 @@ class WtDataHelper:
         @csvFolder  .csv文件的输出目录\n
         @strFilter  代码过滤器(暂未启用)
         '''
-        self.api.dump_bars(bytes(binFolder, encoding="utf8"), bytes(csvFolder, encoding="utf8"), bytes(strFilter, encoding="utf8"), cb_dthelper_log)
+        self.api.dump_bars(bytes(binFolder, encoding="utf8"), bytes(csvFolder, encoding="utf8"), bytes(strFilter, encoding="utf8"), self.cb_dthelper_log)
 
     def dump_ticks(self, binFolder: str, csvFolder: str, strFilter: str=""):
         '''
@@ -67,7 +74,7 @@ class WtDataHelper:
         @csvFolder  .csv文件的输出目录\n
         @strFilter  代码过滤器(暂未启用)
         '''
-        self.api.dump_ticks(bytes(binFolder, encoding="utf8"), bytes(csvFolder, encoding="utf8"), bytes(strFilter, encoding="utf8"), cb_dthelper_log)
+        self.api.dump_ticks(bytes(binFolder, encoding="utf8"), bytes(csvFolder, encoding="utf8"), bytes(strFilter, encoding="utf8"), self.cb_dthelper_log)
 
     def trans_csv_bars(self, csvFolder: str, binFolder: str, period: str):
         '''
@@ -76,7 +83,7 @@ class WtDataHelper:
         @binFolder  .dsb文件存储目录\n
         @period     K线周期，m1-1分钟线，m5-5分钟线，d-日线
         '''
-        self.api.trans_csv_bars(bytes(csvFolder, encoding="utf8"), bytes(binFolder, encoding="utf8"), bytes(period, encoding="utf8"), cb_dthelper_log)
+        self.api.trans_csv_bars(bytes(csvFolder, encoding="utf8"), bytes(binFolder, encoding="utf8"), bytes(period, encoding="utf8"), self.cb_dthelper_log)
 
     def read_dsb_ticks(self, tickFile: str) -> TickList:
         '''
@@ -85,7 +92,7 @@ class WtDataHelper:
         @return     WTSTickStruct的list
         '''
         tick_cache = TickList()
-        if 0 == self.api.read_dsb_ticks(bytes(tickFile, encoding="utf8"), CB_DTHELPER_TICK(tick_cache.on_read_tick), CB_DTHELPER_COUNT(tick_cache.on_data_count), cb_dthelper_log):
+        if 0 == self.api.read_dsb_ticks(bytes(tickFile, encoding="utf8"), CB_DTHELPER_TICK(tick_cache.on_read_tick), CB_DTHELPER_COUNT(tick_cache.on_data_count), self.cb_dthelper_log):
             return None
         else:
             return tick_cache
@@ -97,7 +104,7 @@ class WtDataHelper:
         @return     WTSBarStruct的list
         '''
         bar_cache = BarList()
-        if 0 == self.api.read_dsb_bars(bytes(barFile, encoding="utf8"), CB_DTHELPER_BAR(bar_cache.on_read_bar), CB_DTHELPER_COUNT(bar_cache.on_data_count), cb_dthelper_log):
+        if 0 == self.api.read_dsb_bars(bytes(barFile, encoding="utf8"), CB_DTHELPER_BAR(bar_cache.on_read_bar), CB_DTHELPER_COUNT(bar_cache.on_data_count), self.cb_dthelper_log):
             return None
         else:
             return bar_cache
@@ -109,7 +116,7 @@ class WtDataHelper:
         @return     WTSTickStruct的list
         '''
         tick_cache = TickList()
-        if 0 == self.api.read_dmb_ticks(bytes(tickFile, encoding="utf8"), CB_DTHELPER_TICK(tick_cache.on_read_tick), CB_DTHELPER_COUNT(tick_cache.on_data_count), cb_dthelper_log):
+        if 0 == self.api.read_dmb_ticks(bytes(tickFile, encoding="utf8"), CB_DTHELPER_TICK(tick_cache.on_read_tick), CB_DTHELPER_COUNT(tick_cache.on_data_count), self.cb_dthelper_log):
             return None
         else:
             return tick_cache
@@ -121,7 +128,7 @@ class WtDataHelper:
         @return     WTSBarStruct的list
         '''
         bar_cache = BarList()
-        if 0 == self.api.read_dmb_bars(bytes(barFile, encoding="utf8"), CB_DTHELPER_BAR(bar_cache.on_read_bar), CB_DTHELPER_COUNT(bar_cache.on_data_count), cb_dthelper_log):
+        if 0 == self.api.read_dmb_bars(bytes(barFile, encoding="utf8"), CB_DTHELPER_BAR(bar_cache.on_read_bar), CB_DTHELPER_COUNT(bar_cache.on_data_count), self.cb_dthelper_log):
             return None
         else:
             return bar_cache
@@ -135,7 +142,7 @@ class WtDataHelper:
         @period     周期，m1/m5/d
         '''
         cb = CB_DTHELPER_BAR_GETTER(getter)
-        return self.api.trans_bars(bytes(barFile, encoding="utf8"), cb, count, bytes(period, encoding="utf8"), cb_dthelper_log)
+        return self.api.trans_bars(bytes(barFile, encoding="utf8"), cb, count, bytes(period, encoding="utf8"), self.cb_dthelper_log)
 
     def trans_ticks(self, tickFile:str, getter, count:int) -> bool:
         '''
@@ -145,7 +152,7 @@ class WtDataHelper:
         @count      一共要写入的数据条数
         '''
         cb = CB_DTHELPER_TICK_GETTER(getter)
-        return self.api.trans_ticks(bytes(tickFile, encoding="utf8"), cb, count, cb_dthelper_log)
+        return self.api.trans_ticks(bytes(tickFile, encoding="utf8"), cb, count, self.cb_dthelper_log)
 
     def resample_bars(self, barFile:str, period:str, times:int, fromTime:int, endTime:int, sessInfo:SessionInfo) -> BarList:
         '''
@@ -159,7 +166,7 @@ class WtDataHelper:
         '''
         bar_cache = BarList()
         if 0 == self.api.resample_bars(bytes(barFile, encoding="utf8"), CB_DTHELPER_BAR(bar_cache.on_read_bar), CB_DTHELPER_COUNT(bar_cache.on_data_count), 
-                fromTime, endTime, bytes(period,'utf8'), times, bytes(sessInfo.toString(),'utf8'), cb_dthelper_log):
+                fromTime, endTime, bytes(period,'utf8'), times, bytes(sessInfo.toString(),'utf8'), self.cb_dthelper_log):
             return None
         else:
             return bar_cache
