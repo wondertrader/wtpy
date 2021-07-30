c5940e12ecb02edb79f5cfad13770925f2aafe62
wondertrader <wondertrader@126.com>
2021-07-30 18:26:12 +0800

    [mod]WtDtServo的remote模式改成隐式的

--- wtpy/WtDtServo.py
+++ wtpy/WtDtServo.py
@@ -86,12 +86,12 @@ class WtDtServo:
                 }
             }
 
-    def setRemoteUrl(self, remote_url:str):
+    def setRemoteUrl(self, url:str="http://127.0.0.1:8081"):
         if self.__config__ is not None:
             raise Exception('WtDtServo is already in local mode')
             return
         
-        self.remote_api = WtDtRemoteServo(remote_url)
+        self.remote_api = WtDtRemoteServo(url)
 
 
     def setBasefiles(self, commfile:str="./common/commodities.json", contractfile:str="./common/contracts.json", 
