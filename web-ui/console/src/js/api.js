b40b31c305cd16687a6c4b7d7dabe0a5c6e47c8a
wondertrader <wondertrader@126.com>
2021-08-02 18:03:26 +0800

    [mod]完善了webui对实盘组合的推送数据的支持

--- web-ui/console/src/js/api.js
+++ web-ui/console/src/js/api.js
@@ -28,6 +28,30 @@ Date.prototype.addDays = function (days) {
 	return new Date(d);
 };
 
+String.prototype.format = function (args) {
+    if (arguments.length > 0) {
+        var result = this;
+        if (arguments.length == 1 && typeof (args) == "object") {
+            for (var key in args) {
+                var reg = new RegExp("({" + key + "})", "g");
+                result = result.replace(reg, args[key]);
+            }
+        } else {
+            for (var i = 0; i < arguments.length; i++) {
+                if (arguments[i] == undefined) {
+                    return "";
+                } else {
+                    var reg = new RegExp("({[" + i + "]})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
+        return result;
+    } else {
+        return this;
+    }
+}
+
 
 export default function () {
 
