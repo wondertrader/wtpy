0fe9f9d52735cf32cc68070b6655f3af24f6d985
wondertrader <wondertrader@126.com>
2021-08-11 18:48:12 +0800

    [add]增加了一个回测管理模块WtBtMon.py，暂时还没开始完善

---
+++ wtpy/monitor/WtBtMon.py
@@ -0,0 +1,24 @@
+'''
+Descripttion: 回测管理模块
+version: 
+Author: Wesley
+Date: 2021-08-11 14:03:33
+LastEditors: Wesley
+LastEditTime: 2021-08-11 17:40:57
+'''
+
+class WtBtTask:
+    '''
+    回测任务类
+    '''
+    def __init__(self):
+        pass
+
+class WtBtMon:
+    '''
+    回测管理器
+    '''
+    def __init__(self, path:str):
+        pass
+
+    
