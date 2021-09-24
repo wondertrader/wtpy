b2bf495028ea782e5af4e3236177babec5e9e777
ZerounNet <zerounnet@github.com>
2021-09-24 15:37:46 +0800

    Modified   demos/cta_fut_bt/runTrain.py

--- demos/cta_fut_bt/runTrain.py
+++ demos/cta_fut_bt/runTrain.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from gym import Env
-from gym.spaces import Box
+# from gym import Env
+# from gym.spaces import Box
 
 from wtpy.CtaContext import CtaContext
 from wtpy.StrategyDefs import BaseCtaStrategy
@@ -15,38 +15,29 @@ class EnvStrategy(BaseCtaStrategy):
         self.__period__:str = period
         self.__count__:int = count
 
+        self.obs = 1
+        self.reward = 1
+        self.action = 0
+
     def on_init(self, context:CtaContext):
         #先订阅实时数据
         context.stra_get_bars(self.__code__, self.__period__, self.__count__, True)
 
     def on_calculate(self, context: CtaContext):
-        # 输出 obs和reward
-        # 获得 action
-        pass
+        print('on_calculate action%s'%self.action)
 
-class DemoEnv(Env):
-    def __init__(self) -> None:
-        #这是必须定义的，否则大部分rl框架会检测不通过
-        self.observation_space:Box = Box(low=-np.inf, high=np.inf, shape=(4, ), dtype=np.float64)
-        self.action_space:Box = Box(low=-1, high=1, shape=(1, ), dtype=np.float64)
-        self._iter_ = 0
+        # todo 输出 obs和reward 给外部
+        self.obs += 1
+        self.reward += 1
 
-    def reset(self) -> np.ndarray:
-        self._iter_ += 1
-        return self.observation_space.sample() #随机模拟
 
-    def step(self, action:np.ndarray) -> tuple:
-        obs = self.observation_space.sample() #随机模拟
-        reward = 1 #奖励
-        done = True if np.random.randint(1, 100)==99 else False #是否结束
-        return obs, reward, done, {}
 
-    def close(self) -> None:
-        pass
-
-class WtEnv(DemoEnv):
+class WtEnv():
     def __init__(self) -> None:
         super().__init__()
+
+        self._iter_ = 0
+        self._strategy = None
 
         #创建一个运行环境
         self._engine_:WtBtEngine = WtBtEngine(EngineType.ET_CTA)
@@ -56,19 +47,31 @@ class WtEnv(DemoEnv):
         self._engine_.commitBTConfig()
 
     def reset(self) -> np.ndarray:
+        self.close()
         self._iter_ += 1
+
         #创建一个策略，并加入运行环境
-        strategy = EnvStrategy(name='EnvStrategy_%s'%self._iter_, code='CFFEX.IF.HOT', period='m5', count=60)
-        self._engine_.set_cta_strategy(strategy, slippage=1)
-        self._engine_.run_backtest()
+        self._strategy = EnvStrategy(name='EnvStrategy_%s'%self._iter_, code='CFFEX.IF.HOT', period='m5', count=60)
+        # 设置策略的时候，一定要安装钩子
+        self._engine_.set_cta_strategy(self._strategy, slippage=1, hook=True)
+        # 回测一定要异步运行，不然这里不会返回，回测结束了才会返回
+        self._engine_.run_backtest(bAsync=True)
+
+        self._strategy.action = 0
+        self._engine_.cta_step()
 
         #todo 怎么从on_calc里拿到obs和reward
+        return self._strategy.obs
 
     def step(self, action:np.ndarray) -> tuple:
-        #todo 怎么把action传给on_calc并拿到下次obs和reward
+        #todo  怎么把action传入oncalc里
+        self._strategy.action = action
 
-        obs = self.observation_space.sample() #随机模拟
-        reward = 1 #奖励
+        # 单步触发oncalc
+        self._engine_.cta_step()
+
+        obs = self._strategy.obs # todo 怎么从取得oncalc里的obs数据
+        reward = self._strategy.reward # todo 怎么从取得reward里的obs数据
         done = True if np.random.randint(1, 100)==99 else False #是否结束
         return obs, reward, done, {}
     
@@ -79,18 +82,22 @@ class WtEnv(DemoEnv):
         self._engine_.release_backtest()
 
 if __name__ == '__main__':
-    env = DemoEnv()
-    done = False
-    obs = env.reset()
-    while not done:
-        obs, reward, done, info = env.step(env.action_space.sample)
-        print(obs, reward, done, info)
-    env.close()
+    # env = DemoEnv()
+    # done = False
+    # obs = env.reset()
+    # while not done:
+    #     obs, reward, done, info = env.step(1)
+    #     print(obs, reward, done, info)
+    # env.close()
 
     env = WtEnv()
-    done = False
-    obs = env.reset()
-    # while not done:
-    #     obs, reward, done, info = env.step(env.action_space.sample)
-    #     print(obs, reward, done, info)
+    for i in range(10): #模拟训练10次
+        print('第%s次训练'%i)
+        obs = env.reset()
+        done = False
+        action = 0
+        while not done:
+            action += 1 #模拟智能体产生动作
+            obs, reward, done, info = env.step(action)
+            print('obs%s'%obs, 'reward%s'%reward, done, info)
     env.close()
