5ce8ac8ed87abc71802644167e6f1a35433c5ef3
wondertrader <wondertrader@126.com>
2021-09-26 23:36:03 +0800

    [mod]WtDtEngine支持扩展Parser的接入

--- wtpy/WtDtEngine.py
+++ wtpy/WtDtEngine.py
@@ -1,4 +1,5 @@
 from wtpy.wrapper import WtDtWrapper
+from wtpy.ExtModuleDefs import BaseExtParser
 from wtpy.WtUtilDefs import singleton
 
 @singleton
@@ -6,6 +7,7 @@ class WtDtEngine:
 
     def __init__(self):
         self.__wrapper__ = WtDtWrapper()  #api接口转换器
+        self.__ext_parsers__ = dict()   #外接的行情接入模块
 
     def initialize(self, cfgfile:str = "dtcfg.json", logprofile:str = "logcfgdt.json"):
         '''
@@ -20,3 +22,18 @@ class WtDtEngine:
         运行数据引擎
         '''
         self.__wrapper__.run_datakit()
+
+    def add_exetended_parser(self, parser:BaseExtParser):
+        id = parser.id()
+        if id not in self.__ext_parsers__:
+            self.__ext_parsers__[id] = parser
+            if not self.__wrapper__.create_extended_parser(id):
+                self.__ext_parsers__.pop(id)
+
+    def get_extended_parser(self, id:str)->BaseExtParser:
+        if id not in self.__ext_parsers__:
+            return None
+        return self.__ext_parsers__[id]
+
+    def push_quote_from_extended_parser(self, id:str, newTick, bNeedSlice:bool):
+        self.__wrapper__.push_quote_from_exetended_parser(id, newTick, bNeedSlice)
