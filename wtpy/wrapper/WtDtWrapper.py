1cd6e68e54e0a1ff27eb67923a3f1273c3d18fef
wondertrader <wondertrader@126.com>
2021-09-26 23:36:03 +0800

    [mod]WtDtEngine支持扩展Parser的接入

--- wtpy/wrapper/WtDtWrapper.py
+++ wtpy/wrapper/WtDtWrapper.py
@@ -6,9 +6,11 @@ Date: 2021-07-27 09:53:43
 LastEditors: Wesley
 LastEditTime: 2021-08-13 15:26:16
 '''
-from ctypes import cdll, c_char_p
+from ctypes import cdll, c_char_p, c_bool, POINTER
 from .PlatformHelper import PlatformHelper as ph
 from wtpy.WtUtilDefs import singleton
+from wtpy.WtCoreDefs import WTSTickStruct, CB_PARSER_EVENT, CB_PARSER_SUBCMD
+from wtpy.WtCoreDefs import EVENT_PARSER_CONNECT, EVENT_PARSER_DISCONNECT, EVENT_PARSER_INIT, EVENT_PARSER_RELEASE
 import os
 
 # Python对接C接口的库
@@ -32,6 +34,9 @@ class WtDtWrapper:
         self.api.get_version.restype = c_char_p
         self.ver = bytes.decode(self.api.get_version())
 
+        self.api.create_ext_parser.restype = c_bool
+        self.api.create_ext_parser.argtypes = [c_char_p]
+
     def run_datakit(self):
         '''
         启动数据组件
@@ -54,3 +59,44 @@ class WtDtWrapper:
             print(oe)
 
         self.write_log(102, "WonderTrader datakit initialzied，version：%s" % (self.ver))
+
+    def create_extended_parser(self, id:str) -> bool:
+        return self.api.create_ext_parser(bytes(id, encoding = "utf8"))
+
+    def push_quote_from_exetended_parser(self, id:str, newTick:POINTER(WTSTickStruct), bNeedSlice:bool = True):
+        return self.api.parser_push_quote(bytes(id, encoding = "utf8"), newTick, bNeedSlice)
+
+    def register_extended_module_callbacks(self,):
+        self.cb_parser_event = CB_PARSER_EVENT(self.on_parser_event)
+        self.cb_parser_subcmd = CB_PARSER_SUBCMD(self.on_parser_sub)
+
+        self.api.register_parser_callbacks(self.cb_parser_event, self.cb_parser_subcmd)
+        self.api.register_exec_callbacks(self.cb_executer_init, self.cb_executer_cmd)
+
+    def on_parser_event(self, evtId:int, id:str):
+        id = bytes.decode(id)
+        engine = self._engine
+        parser = engine.get_extended_parser(id)
+        if parser is None:
+            return
+        
+        if evtId == EVENT_PARSER_INIT:
+            parser.init(engine)
+        elif evtId == EVENT_PARSER_CONNECT:
+            parser.connect()
+        elif evtId == EVENT_PARSER_DISCONNECT:
+            parser.disconnect()
+        elif evtId == EVENT_PARSER_RELEASE:
+            parser.release()
+
+    def on_parser_sub(self, id:str, fullCode:str, isForSub:bool):
+        id = bytes.decode(id)
+        engine = self._engine
+        parser = engine.get_extended_parser(id)
+        if parser is None:
+            return
+        fullCode = bytes.decode(fullCode)
+        if isForSub:
+            parser.subscribe(fullCode)
+        else:
+            parser.unsubscribe(fullCode)
