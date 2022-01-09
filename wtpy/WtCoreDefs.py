ae17a2fbb815479b5a4d1bc98f64790d3565c692
zerounnet <zerounnet@github.com>
2022-01-09 17:07:36 +0800

    [bug]WTSBarStruct的to_tuple方法需要添加reserve字段

--- wtpy/WtCoreDefs.py
+++ wtpy/WtCoreDefs.py
@@ -123,6 +123,7 @@ class WTSBarStruct(WTSStruct):
     def to_tuple(self, isDays:bool=False) -> tuple:
         return (
                 self.date,
+                self.reserve,
                 self.date if isDays else self.time + 199000000000,
                 self.open,
                 self.high,
