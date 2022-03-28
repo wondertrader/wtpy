328d01486ed2b3b34546091893613ca4b47f5e62
wondertrader <wondertrader@126.com>
2022-03-28 16:57:12 +0800

    [mod]版本号改成0.9.2，底层C++二进制文件更新到v0.9.2版本

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/wondertrader/wtpy'
 EMAIL = 'silencesword@foxmail.com'
 AUTHOR = 'Wesley Liu'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
