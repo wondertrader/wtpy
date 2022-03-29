d81fcbe1fc45aa9ca620eaf524b65d858808a620
wondertrader <wondertrader@126.com>
2022-03-29 21:21:23 +0800

    [mod]修改版本号为0.9.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/wondertrader/wtpy'
 EMAIL = 'silencesword@foxmail.com'
 AUTHOR = 'Wesley Liu'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '0.9.2'
+VERSION = '0.9.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
