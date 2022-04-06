58d0196e7603c60ded35c5a369d88787192d934e
wondertrader <wondertrader@126.com>
2022-04-06 23:39:47 +0800

    [mod]runBT.py中增加了调用pyfolio进行绩效分析的演示代码

--- demos/cta_fut_bt/runBT.py
+++ demos/cta_fut_bt/runBT.py
@@ -1,7 +1,26 @@
 from wtpy import WtBtEngine,EngineType
-from wtpy.apps import WtBtAnalyst
-
 from Strategies.DualThrust import StraDualThrust
+
+def analyze_with_pyfolio(fund_filename:str, capital:float=500000):
+    import pyfolio as pf
+    import pandas as pd
+    from datetime import datetime
+    import matplotlib.pyplot as plt
+
+    # 读取每日资金
+    df = pd.read_csv(fund_filename)
+    df['date'] = df['date'].apply(lambda x : datetime.strptime(str(x), '%Y%m%d'))
+    df = df.set_index(df["date"])
+
+    # 将资金转换成收益率
+    ay = df['dynbalance'] + capital
+    rets = ay.pct_change().fillna(0).tz_localize('UTC')
+
+    # 调用pyfolio进行分析
+    pf.create_full_tear_sheet(rets)
+
+    # 如果在jupyter，不需要执行该语句
+    plt.show()
 
 if __name__ == "__main__":
     #创建一个运行环境，并加入策略
@@ -28,12 +47,16 @@ if __name__ == "__main__":
     #开始运行回测
     engine.run_backtest(bAsync=False)
 
-    #创建绩效分析模块
-    analyst = WtBtAnalyst()
-    #将回测的输出数据目录传递给绩效分析模块
-    analyst.add_strategy("pydt_IF", folder="./outputs_bt/pydt_IF/", init_capital=500000, rf=0.02, annual_trading_days=240)
-    #运行绩效模块
-    analyst.run_new()
+    if True:
+        #创建绩效分析模块
+        analyst = WtBtAnalyst()
+        #将回测的输出数据目录传递给绩效分析模块
+        analyst.add_strategy("pydt_IF", folder="./outputs_bt/pydt_IF/", init_capital=500000, rf=0.02, annual_trading_days=240)
+        #运行绩效模块
+        analyst.run_new()
+    else:
+        #使用pyfolio进行绩效分析
+        analyze_with_pyfolio("./outputs_bt/pydt_IF/funds.csv")
 
     kw = input('press any key to exit\n')
     engine.release_backtest()
