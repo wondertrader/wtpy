a8621b3daa4ed2109dec25ba1da3982a5f6d6ec0
HeJ <huerjie1996@foxmail.com>
2022-06-22 17:31:41 +0800

    [mod] 增加回调中的none容错

--- wtpy/wrapper/WtBtWrapper.py
+++ wtpy/wrapper/WtBtWrapper.py
@@ -1,4 +1,4 @@
-from ctypes import cdll, c_char_p, c_bool, c_ulong, c_uint64, c_double, POINTER, addressof, sizeof
+from ctypes import cdll, c_char_p, c_bool, c_ulong, c_uint64, c_double, c_int, POINTER, addressof, sizeof
 from wtpy.WtCoreDefs import CB_STRATEGY_INIT, CB_STRATEGY_TICK, CB_STRATEGY_CALC, CB_STRATEGY_BAR, CB_STRATEGY_GET_BAR, CB_STRATEGY_GET_TICK, CB_STRATEGY_GET_POSITION
 from wtpy.WtCoreDefs import CB_HFTSTRA_CHNL_EVT, CB_HFTSTRA_ENTRUST, CB_HFTSTRA_ORD, CB_HFTSTRA_TRD, CB_SESSION_EVENT
 from wtpy.WtCoreDefs import CB_HFTSTRA_ORDQUE, CB_HFTSTRA_ORDDTL, CB_HFTSTRA_TRANS, CB_HFTSTRA_GET_ORDQUE, CB_HFTSTRA_GET_ORDDTL, CB_HFTSTRA_GET_TRANS
@@ -53,6 +53,7 @@ class WtBtWrapper:
         self.api.cta_get_detail_cost.restype = c_double
         self.api.cta_get_detail_profit.restype = c_double
         self.api.cta_get_price.restype = c_double
+        self.api.cta_get_day_price.restype = c_double
         self.api.cta_get_fund_data.restype = c_double
 
         self.api.sel_save_userdata.argtypes = [c_ulong, c_char_p, c_char_p]
@@ -66,12 +67,13 @@ class WtBtWrapper:
         self.api.hft_load_userdata.restype = c_char_p
         self.api.hft_get_position.restype = c_double
         self.api.hft_get_position_profit.restype = c_double
+        self.api.hft_get_position_avgpx.restype = c_double
         self.api.hft_get_undone.restype = c_double
         
         self.api.hft_buy.restype = c_char_p
-        self.api.hft_buy.argtypes = [c_ulong, c_char_p, c_double, c_double, c_char_p]
+        self.api.hft_buy.argtypes = [c_ulong, c_char_p, c_double, c_double, c_char_p, c_int]
         self.api.hft_sell.restype = c_char_p
-        self.api.hft_sell.argtypes = [c_ulong, c_char_p, c_double, c_double, c_char_p]
+        self.api.hft_sell.argtypes = [c_ulong, c_char_p, c_double, c_double, c_char_p, c_int]
         # 回测不需要 self.api.hft_cancel_all.restype = c_char_p
 
         self.api.set_time_range.argtypes = [c_uint64, c_uint64]
@@ -226,36 +228,6 @@ class WtBtWrapper:
         ticks = [None]*count # 预先分配list的长度
         for idx in range(count):
             realTick = WTSTickStruct.from_address(addr)   # 从内存中直接解析成WTSTickStruct
-            '''
-            tick = dict()
-            tick["time"] = realTick.action_date * 1000000000 + realTick.action_time
-            tick["open"] = realTick.open
-            tick["high"] = realTick.high
-            tick["low"] = realTick.low
-            tick["price"] = realTick.price
-
-            tick["bidprice"] = list()
-            tick["bidqty"] = list()
-            tick["askprice"] = list()
-            tick["askqty"] = list()
-            
-            tick["total_volume"] = realTick.total_volume
-            tick["volume"] = realTick.volume
-            tick["total_turnover"] = realTick.total_turnover
-            tick["turn_over"] = realTick.turn_over
-            tick["open_interest"] = realTick.open_interest
-            tick["diff_interest"] = realTick.diff_interest
-
-            for i in range(10):
-                if realTick.bid_qty[i] != 0:
-                    tick["bidprice"].append(realTick.bid_prices[i])
-                    tick["bidqty"].append(realTick.bid_qty[i])
-
-                if realTick.ask_qty[i] != 0:
-                    tick["askprice"].append(realTick.ask_prices[i])
-                    tick["askqty"].append(realTick.ask_qty[i])
-            '''
-
             ticks[idx] = realTick.to_tuple()
             addr += tsSize
 
@@ -271,7 +243,8 @@ class WtBtWrapper:
     def on_hftstra_channel_evt(self, id:int, trader:str, evtid:int):
         engine = self._engine
         ctx = engine.get_context(id)
-        
+        if ctx is None:
+            return
         if evtid == CHNL_EVENT_READY:
             ctx.on_channel_ready()
         elif evtid == CHNL_EVENT_LOST:
@@ -279,45 +252,34 @@ class WtBtWrapper:
 
     def on_hftstra_order(self, id:int, localid:int, stdCode:str, isBuy:bool, totalQty:float, leftQty:float, price:float, isCanceled:bool, userTag:str):
         stdCode = bytes.decode(stdCode)
-        userTag = bytes.decode(userTag)
+        userTag = bytes.decode(userTag,"gbk")
         engine = self._engine
         ctx = engine.get_context(id)
-        ctx.on_order(localid, stdCode, isBuy, totalQty, leftQty, price, isCanceled, userTag)
+        if ctx is not None:
+            ctx.on_order(localid, stdCode, isBuy, totalQty, leftQty, price, isCanceled, userTag)
 
     def on_hftstra_trade(self, id:int, localid:int, stdCode:str, isBuy:bool, qty:float, price:float, userTag:str):
         stdCode = bytes.decode(stdCode)
-        userTag = bytes.decode(userTag)
+        userTag = bytes.decode(userTag,"gbk")
         engine = self._engine
         ctx = engine.get_context(id)
-        ctx.on_trade(localid, stdCode, isBuy, qty, price, userTag)
+        if ctx is not None:
+            ctx.on_trade(localid, stdCode, isBuy, qty, price, userTag)
 
     def on_hftstra_entrust(self, id:int, localid:int, stdCode:str, bSucc:bool, message:str, userTag:str):
         stdCode = bytes.decode(stdCode)
         message = bytes.decode(message, "gbk")
-        userTag = bytes.decode(userTag)
+        userTag = bytes.decode(userTag, "gbk")
         engine = self._engine
         ctx = engine.get_context(id)
-        ctx.on_entrust(localid, stdCode, bSucc, message, userTag)
+        if ctx is not None:
+            ctx.on_entrust(localid, stdCode, bSucc, message, userTag)
 
     def on_hftstra_order_queue(self, id:int, stdCode:str, newOrdQue:POINTER(WTSOrdQueStruct)):
         stdCode = bytes.decode(stdCode)
         engine = self._engine
         ctx = engine.get_context(id)
-        newOrdQue = newOrdQue.contents
-        # curOrdQue = dict()
-        # curOrdQue["time"] = newOrdQue.action_date * 1000000000 + newOrdQue.action_time
-        # curOrdQue["side"] = newOrdQue.side
-        # curOrdQue["price"] = newOrdQue.price
-        # curOrdQue["order_items"] = newOrdQue.order_items
-        # curOrdQue["qsize"] = newOrdQue.qsize
-        # curOrdQue["volumes"] = list()
-
-        # for i in range(50):
-        #     if newOrdQue.volumes[i] == 0:
-        #         break
-        #     else:
-        #         curOrdQue["volumes"].append(newOrdQue.volumes[i])
-        
+        newOrdQue = newOrdQue.contents        
         if ctx is not None:
             ctx.on_order_queue(stdCode, newOrdQue.to_tuple())
 
@@ -329,20 +291,6 @@ class WtBtWrapper:
         item_list = [None]*count
         for i in range(count):
             realOrdQue = WTSOrdQueStruct.from_address(addr)
-            # curOrdQue = dict()
-            # curOrdQue["time"] = realOrdQue.action_date * 1000000000 + realOrdQue.action_time
-            # curOrdQue["side"] = realOrdQue.side
-            # curOrdQue["price"] = realOrdQue.price
-            # curOrdQue["order_items"] = realOrdQue.order_items
-            # curOrdQue["qsize"] = realOrdQue.qsize
-            # curOrdQue["volumes"] = list()
-
-            # for i in range(50):
-            #     if realOrdQue.volumes[i] == 0:
-            #         break
-            #     else:
-            #         curOrdQue["volumes"].append(realOrdQue.volumes[i])
-
             item_list[i] = realOrdQue.to_tuple()
             addr += szItem
             
@@ -353,14 +301,6 @@ class WtBtWrapper:
         engine = self._engine
         ctx = engine.get_context(id)
         newOrdDtl = newOrdDtl.contents
-
-        # curOrdDtl = dict()
-        # curOrdDtl["time"] = newOrdDtl.action_date * 1000000000 + newOrdDtl.action_time
-        # curOrdDtl["index"] = newOrdDtl.index
-        # curOrdDtl["side"] = newOrdDtl.side
-        # curOrdDtl["price"] = newOrdDtl.price
-        # curOrdDtl["volume"] = newOrdDtl.volume
-        # curOrdDtl["otype"] = newOrdDtl.otype
         
         if ctx is not None:
             ctx.on_order_detail(stdCode, newOrdDtl.to_tuple())
@@ -373,13 +313,6 @@ class WtBtWrapper:
         item_list = [None]*count
         for i in range(count):
             realOrdDtl = WTSOrdDtlStruct.from_address(addr)
-            # curOrdDtl = dict()
-            # curOrdDtl["time"] = realOrdDtl.action_date * 1000000000 + realOrdDtl.action_time
-            # curOrdDtl["index"] = realOrdDtl.index
-            # curOrdDtl["side"] = realOrdDtl.side
-            # curOrdDtl["price"] = realOrdDtl.price
-            # curOrdDtl["volume"] = realOrdDtl.volume
-            # curOrdDtl["otype"] = realOrdDtl.otype
 
             item_list[i] = realOrdDtl.to_tuple()
             addr += szItem
@@ -391,16 +324,6 @@ class WtBtWrapper:
         engine = self._engine
         ctx = engine.get_context(id)
         newTrans = newTrans.contents
-
-        # curTrans = dict()
-        # curTrans["time"] = newTrans.action_date * 1000000000 + newTrans.action_time
-        # curTrans["index"] = newTrans.index
-        # curTrans["ttype"] = newTrans.ttype
-        # curTrans["side"] = newTrans.side
-        # curTrans["price"] = newTrans.price
-        # curTrans["volume"] = newTrans.volume
-        # curTrans["askorder"] = newTrans.askorder
-        # curTrans["bidorder"] = newTrans.bidorder
         
         if ctx is not None:
             ctx.on_transaction(stdCode, newTrans.to_tuple())
@@ -413,15 +336,6 @@ class WtBtWrapper:
         trans_list = [None]*count
         for i in range(count):
             realTrans = WTSTransStruct.from_address(addr)
-            # curTrans = dict()
-            # curTrans["time"] = realTrans.action_date * 1000000000 + realTrans.action_time
-            # curTrans["index"] = realTrans.index
-            # curTrans["ttype"] = realTrans.ttype
-            # curTrans["side"] = realTrans.side
-            # curTrans["price"] = realTrans.price
-            # curTrans["volume"] = realTrans.volume
-            # curTrans["askorder"] = realTrans.askorder
-            # curTrans["bidorder"] = realTrans.bidorder
             trans_list[i] = realTrans.to_tuple()
             addr += szTrans
             
@@ -434,7 +348,6 @@ class WtBtWrapper:
         if loader is None:
             return False
 
-        # feed_raw_bars(WTSBarStruct* bars, WtUInt32 count);
         return loader.load_final_his_bars(bytes.decode(stdCode), bytes.decode(period), self.api.feed_raw_bars)
 
     def on_load_raw_his_bars(self, stdCode:str, period:str) -> bool:
@@ -443,7 +356,6 @@ class WtBtWrapper:
         if loader is None:
             return False
 
-        # feed_raw_bars(WTSBarStruct* bars, WtUInt32 count);
         return loader.load_raw_his_bars(bytes.decode(stdCode), bytes.decode(period), self.api.feed_raw_bars)
 
     def feed_adj_factors(self, stdCode:str, dates:list, factors:list):
@@ -502,6 +414,9 @@ class WtBtWrapper:
 
     def clear_cache(self):
         self.api.clear_cache()
+
+    def get_raw_stdcode(self, stdCode:str):
+        return bytes.decode(self.api.get_raw_stdcode(bytes(stdCode, encoding = "utf8")))
 
     def config_backtest(self, cfgfile:str = 'config.yaml', isFile:bool = True):
         self.api.config_backtest(bytes(cfgfile, encoding = "utf8"), isFile)
@@ -701,6 +616,15 @@ class WtBtWrapper:
         '''
         return self.api.cta_get_price(bytes(stdCode, encoding = "utf8"))
 
+    def cta_get_day_price(self, stdCode:str, flag:int = 0) -> float:
+        '''
+        获取当日价格
+        @stdCode    合约代码
+        @flag       价格标记，0-开盘价，1-最高价，2-最低价，3-最新价
+        @return     指定合约的价格 
+        '''
+        return self.api.cta_get_day_price(bytes(stdCode, encoding = "utf8"), flag)
+
     def cta_set_position(self, id:int, stdCode:str, qty:float, usertag:str = "", limitprice:float = 0.0, stopprice:float = 0.0):
         '''
         设置目标仓位
@@ -755,13 +679,14 @@ class WtBtWrapper:
         '''
         return self.api.cta_get_last_exittime(id, bytes(stdCode, encoding = "utf8"))
 
-    def cta_log_text(self, id:int, message:str):
+    def cta_log_text(self, id:int, level:int, message:str):
         '''
         日志输出
         @id         策略ID
+        @level      日志级别
         @message    日志内容
         '''
-        self.api.cta_log_text(id, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
+        self.api.cta_log_text(id, level, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
 
     def cta_get_detail_entertime(self, id:int, stdCode:str, usertag:str) -> int:
         '''
@@ -916,13 +841,14 @@ class WtBtWrapper:
         '''
         return self.api.sel_get_time()
 
-    def sel_log_text(self, id:int, message:str):
+    def sel_log_text(self, id:int, level:int, message:str):
         '''
         日志输出
         @id         策略ID
+        @level      日志级别
         @message    日志内容
         '''
-        self.api.sel_log_text(id, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
+        self.api.sel_log_text(id, level, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
 
     def sel_sub_ticks(self, id:int, stdCode:str):
         '''
@@ -1017,6 +943,15 @@ class WtBtWrapper:
         '''
         return self.api.hft_get_position_profit(id, bytes(stdCode, encoding = "utf8"))
 
+    def hft_get_position_avgpx(self, id:int, stdCode:str):
+        '''
+        获取持仓均价
+        @id     策略id
+        @stdCode   合约代码
+        @return 指定持仓的浮动盈亏
+        '''
+        return self.api.hft_get_position_avgpx(id, bytes(stdCode, encoding = "utf8"))
+
     def hft_get_undone(self, id:int, stdCode:str):
         '''
         获取持仓
@@ -1054,13 +989,14 @@ class WtBtWrapper:
         '''
         return self.api.hft_get_secs()
 
-    def hft_log_text(self, id:int, message:str):
+    def hft_log_text(self, id:int, level:int, message:str):
         '''
         日志输出
         @id         策略ID
+        @level      日志级别
         @message    日志内容
         '''
-        self.api.hft_log_text(id, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
+        self.api.hft_log_text(id, level, bytes(message, encoding = "utf8").decode('utf-8').encode('gbk'))
 
     def hft_sub_ticks(self, id:int, stdCode:str):
         '''
@@ -1112,7 +1048,7 @@ class WtBtWrapper:
         ret = self.api.hft_cancel_all(id, bytes(stdCode, encoding = "utf8"), isBuy)
         return bytes.decode(ret)
 
-    def hft_buy(self, id:int, stdCode:str, price:float, qty:float, userTag:str):
+    def hft_buy(self, id:int, stdCode:str, price:float, qty:float, userTag:str, flag:int):
         '''
         买入指令
         @id         策略ID
@@ -1120,10 +1056,10 @@ class WtBtWrapper:
         @price      买入价格, 0为市价
         @qty        买入数量
         '''
-        ret = self.api.hft_buy(id, bytes(stdCode, encoding = "utf8"), price, qty, bytes(userTag, encoding = "utf8"))
+        ret = self.api.hft_buy(id, bytes(stdCode, encoding = "utf8"), price, qty, bytes(userTag, encoding = "utf8"), flag)
         return bytes.decode(ret)
 
-    def hft_sell(self, id:int, stdCode:str, price:float, qty:float, userTag:str):
+    def hft_sell(self, id:int, stdCode:str, price:float, qty:float, userTag:str, flag:int):
         '''
         卖出指令
         @id         策略ID
@@ -1131,7 +1067,7 @@ class WtBtWrapper:
         @price      卖出价格, 0为市价
         @qty        卖出数量
         '''
-        ret = self.api.hft_sell(id, bytes(stdCode, encoding = "utf8"), price, qty, bytes(userTag, encoding = "utf8"))
+        ret = self.api.hft_sell(id, bytes(stdCode, encoding = "utf8"), price, qty, bytes(userTag, encoding = "utf8"), flag)
         return bytes.decode(ret)
 
     def hft_step(self, id:int):
