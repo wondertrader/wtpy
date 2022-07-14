04ba1394c2d3aa84c2316f68bc32f84e5164fa4c
wondertrader <wondertrader@126.com>
2022-07-14 16:21:11 +0800

    [add]monitor下新增一个WtBtSnooper模块，可以运行一个简单的回测结果提取服务

--- wtpy/monitor/__init__.py
+++ wtpy/monitor/__init__.py
@@ -9,5 +9,6 @@ LastEditTime: 2021-08-16 17:29:42
 from .WtMonSvr import WtMonSvr, WtMonSink
 from .WtBtMon import WtBtMon
 from .WtLogger import WtLogger
+from .WtBtSnooper import WtBtSnooper
 
-__all__ = ["WtMonSvr","WtBtMon","WtLogger", "WtMonSink"]
+__all__ = ["WtMonSvr","WtBtMon","WtLogger", "WtMonSink", "WtBtSnooper"]
