633697d760e03ec474830cf18c55112eb7c9e16b
wondertrader <wondertrader@126.com>
2022-08-01 12:42:37 +0800

    [mod]demos/test_dtservo/testDtServo.py增加一个读取tick数据的示例

--- demos/test_dtservo/testDtServo.py
+++ demos/test_dtservo/testDtServo.py
@@ -15,3 +15,7 @@ bars.to_csv("CFFEX.IF.HOT+.csv")
 # 读取IF主力合约的原始拼接数据
 bars = dtServo.get_bars("CFFEX.IF.HOT", "m5", fromTime=202205010930, endTime=202205281500).to_df()
 bars.to_csv("CFFEX.IF.HOT.csv")
+
+# 读取IF主力合约的tick数据
+bars = dtServo.get_ticks("CFFEX.IF.HOT", fromTime=202207250930, endTime=202207291500).to_df()
+bars.to_csv("CFFEX.IF.HOT_ticks.csv")
