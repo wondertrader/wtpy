ad8554cb8429a3c13a903ac00299fa5f35e08b1f
wondertrader <wondertrader@126>
2023-04-04 15:19:59 +0800

    [mod]版本号改成0.9.7.2；build.bat脚本增加了删除历史构建文件的命令行

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/wondertrader/wtpy'
 EMAIL = 'silencesword@foxmail.com'
 AUTHOR = 'Wesley Liu'
 REQUIRES_PYTHON = '>=3.8.0'
-VERSION = '0.9.7'
+VERSION = '0.9.8'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
